adef17b39b1817a013170792b70d235f384eab1d
ViViHarmonia <153961252+ViViHarmonia@users.noreply.github.com>
2024-07-09 15:39:23 -0600

    Update IndexPage.vue

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -13,33 +13,37 @@ export default defineComponent({
   //@keyup.left="museSwitch(1)" @keyup.right="museSwitch(2)" @keyup.down="slideKeyboard(2)" @keyup.up="slideKeyboard(1)" @keyup.x="museDialog = false"
   methods: {
     placeholderKinkFillText() {
-      this.muse.Name = ""
-      this.muse.NameSh = ""
-      this.muse.SubDom = ""
-      this.muse.SubDomSh = ""
-      this.muse.Uni = ""
-      this.muse.UniSh = ""
-      this.muse.Spec = ""
-      this.muse.SpecSh = ""
-      this.muse.Reg = ""
-      this.muse.RegSh = ""
-      this.muse.Desc = ""
-      this.muse.DescSh = ""
-      this.muse.auArray = [{ au: 'base', emblem: '/versions/mirror.jpg', check: false }]
-      this.muse.slideArray = []
-      this.muse.kinks.partner = "CHANGE"
-      this.muse.kinks.organ = "CHANGE"
-      this.muse.kinks.clothing = "CHANGE"
-      this.muse.kinks.relation = "CHANGE"
-      this.muse.kinks.consent = "CHANGE"
-      this.muse.kinks.substance = "CHANGE"
-      this.muse.kinks.treatment = "CHANGE"
-      this.muse.kinks.bondage = "CHANGE"
-      this.muse.kinks.mindMod = "CHANGE"
-      this.muse.kinks.bodyMod = "CHANGE"
-      this.muse.kinks.transform = "CHANGE"
-
-      this.muse.auArray = [{ au: 'base', emblem: '/versions/mirror.jpg', check: false }]
+      this.muse.Name = "";
+      this.muse.NameSh = "";
+      this.muse.SubDom = "";
+      this.muse.SubDomSh = "";
+      this.muse.Title = "";
+      this.muse.TitleSh = "";
+      this.muse.Spec = "";
+      this.muse.SpecSh = "";
+      this.muse.Reg = "";
+      this.muse.RegSh = "";
+      this.muse.Desc = "";
+      this.muse.DescLewd = "";
+      this.muse.kinks.partner = ""
+      this.muse.kinks.organ = ""
+      this.muse.kinks.clothing = ""
+      this.muse.kinks.relation = ""
+      this.muse.kinks.consent = ""
+      this.muse.kinks.substance = ""
+      this.muse.kinks.treatment = ""
+      this.muse.kinks.bondage = ""
+      this.muse.kinks.mindMod = ""
+      this.muse.kinks.bodyMod = ""
+      this.muse.kinks.transform = ""
+      this.muse.auArray = [
+        {
+          au: "base",
+          emblem: "/versions/mirror.jpg",
+          check: false,
+          sfw: "Y",
+        },
+      ];
 
     },
   },
